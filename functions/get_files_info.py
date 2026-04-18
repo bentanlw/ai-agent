d249113923bf0472519f94ef2d4ef052a37cedfd
bentanlw <bentlw@gmail.com>
2026-04-18 20:05:46 +0800

    added calculator project for ai agent to work on, added functions to get file info from directories, added tests for get_files function

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        abs_work_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_work_dir, directory))
+        valid_target_dir = os.path.commonpath([abs_work_dir, target_dir]) == abs_work_dir
+        if not valid_target_dir:
+            print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            print(f'Error: "{directory}" is not a directory')
+            return f'Error: "{directory}" is not a directory'
+
+        with os.scandir(target_dir) as d:
+            print(f"Result for '{directory}':")
+            for f in d:
+                print(f"- {f.name}: file_size={f.stat().st_size}, is_dir={f.is_dir()}")
+
+    except Exception as e:
+        print(f'Error: Standard Library encountered {e}')
--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+def call_gemini_with_retry(client, messages, retries = 5):
+    for i in range(retries):
+        try:
+            response = client.models.generate_content(
+                    model = 'gemini-2.5-flash-lite',
+                    contents = messages 
+                    )
+        except Exception as e:
+            if "503" in str(e) and i < retries -1:
+                wait_time = (2 ** i) # Waits 1, 2, 4, 8 seconds
+                print(f"Server overloaded. Retrying in {wait_time}s...")
+                time.sleep(wwait_time)
+                continue
+            raise e
+        return response
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("no API key found!")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = call_gemini_with_retry(client, messages)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("failed API request!")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+    print(response.text)
 
 if __name__ == "__main__":
     main()
